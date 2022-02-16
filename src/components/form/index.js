72a86ecacc4ee2c43c180f5b50777f8295f1735a
Kellen-Linse <kblinse06@gmail.com>
2022-02-15 16:45:28 -0800

    add base state functionality

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './form.scss';
 
 function Form(props){
+  
+  const [method, setMethod] = useState('get');
+
+  const methodSetter = (method) => {
+    setMethod(method);
+  }
 
   const handleSubmit = e => {
     e.preventDefault();
     const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      method: method,
+      url: e.target.url.value,
+      body: e.target.body ? e.target.body.value : '',
     };
     props.handleApiCall(formData);
   }
 
+  const clickCheck = (spanMethod) => {
+    return method === spanMethod ? "buttonTrue": "buttonFalse"
+  }
+
+
+
     return (
       <>
         <form onSubmit={handleSubmit}>
+          <label method={method} className="methods">
+            <span id="get"    className={clickCheck('get')}    onClick={() => methodSetter('get')}>GET</span>
+            <span id="post"   className={clickCheck('post')}   onClick={() => methodSetter('post')}>POST</span>
+            <span id="put"    className={clickCheck('put')}    onClick={() => methodSetter('put')}>PUT</span>
+            <span id="delete" className={clickCheck('delete')} onClick={() => methodSetter('delete')}>DELETE</span>
+          </label>
           <label >
             <span>URL: </span>
             <input name='url' type='text' />
             <button type="submit">GO!</button>
           </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
+          {(method === 'post' || method === 'put') && <label>
+              <textarea name="body"></textarea>
+            </label>}
         </form>
       </>
     );
--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import ReactJson from 'react-json-view';
+
+import './results.scss';
 
 function Results(props){
     return (
       <section>
-        <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+        <pre style={{ "height": "400px", "display": "flex", "justifyContent": "space-around"}} >{props.data ? <ReactJson style={{"width": "35%", "display": "flex", "justifyContent": "space-around"}} name="data" src={props.data} theme="monokai" /> : null}</pre>
       </section>
     );
 }
