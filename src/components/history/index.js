2f3ce488984e8a1de65abcfc0d6d1dc3f41312c1
Kellen-Linse <kblinse06@gmail.com>
2022-02-19 21:53:50 -0800

    Add date info to history

--- src/components/history/index.js
+++ src/components/history/index.js
@@ -6,7 +6,7 @@ export default function History({searchHistory, deleteHistory, showHistory}) {
     <>
       <div>History: </div>
       <ul>{searchHistory.map(history => {
-        return <li key={history.id} onClick={() => showHistory(history.id)}>{history.request.method.toUpperCase()} {history.request.url} <button onClick={() => deleteHistory(history.id)}>Delete</button></li>
+        return <li key={history.id} onClick={() => showHistory(history.id)}>{history.request.method.toUpperCase()} {history.request.url} Time: {history.date.getHours()}:{history.date.getMinutes()}:{history.date.getSeconds()} Date: {history.date.getMonth()} / {history.date.getDate()} / {history.date.getFullYear()}  <button onClick={() => deleteHistory(history.id)}>Delete</button></li>
       })}</ul>
     </>
   )
